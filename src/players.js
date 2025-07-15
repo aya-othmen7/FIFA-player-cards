60a4f8668f3e8806ca3e772491d3b9ffb7031227
aya-othmen7 <ayaothmen7@gmail.com>
2025-07-15 11:32:19 +0100

    Add players.js with 4 player objects

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/89/Leo_Messi_20180626.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al-Nassr",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 39,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Real Madrid",
+    nationality: "France",
+    jerseyNumber: 9,
+    age: 25,
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a8/Kylian_Mbapp%C3%A9_2018.jpg"
+  },
+  {
+    name: "Mohamed Salah",
+    team: "Liverpool",
+    nationality: "Egypt",
+    jerseyNumber: 11,
+    age: 32,
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Mohamed_Salah_in_2018.jpg"
+  }
+];
+
+export default players;
